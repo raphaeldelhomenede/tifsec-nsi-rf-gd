d6c31b2c03afd76e01ed63f4801ed16ffa875737
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-04-30 19:34:02 +0200

    Update sujet 2 exercice 1 - cirrigé.py

--- TNSI/seances/pratique/html/consignes/sujets/corrigé/sujet 2 exercice 1 - cirrigé.py
+++ TNSI/seances/pratique/html/consignes/sujets/corrigé/sujet 2 exercice 1 - cirrigé.py
@@ -1,5 +1,5 @@
 def renverse(chaine_de_caracteres):
-    if chaine_de_caracteres is not str:
+    if type(chaine_de_caracteres) is not str:
         return "Ceci n'est pas une chaîne de caractères!"
     else:
         return chaine_de_caracteres[::-1]
