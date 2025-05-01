566a3090660831c4b6e8961ca7330cb486a15965
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-05-01 16:11:12 +0200

    Add files via upload

---
+++ TNSI/seances/pratique/html/consignes/sujets/sujet 4 exercice 2.py
@@ -0,0 +1,14 @@
+""" Les consignes sont sur https://tifsec-nsi.rf.gd/TNSI/?session=Sujet4Exercice2 """
+def crible(n):
+    """Renvoie un tableau contenant tous les nombres premiers plus petits que n."""
+    premiers = []
+    tab = [True] * n
+    tab[0], tab[1] = False, False
+    for i in range(n):
+        if tab[i]:
+            premiers...
+            multiple = ...
+            while multiple < n:
+                tab[multiple] = ...
+                multiple = ...
+    return premiers
