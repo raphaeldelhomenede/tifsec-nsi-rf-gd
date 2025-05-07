4f52abfcf922917259559e995a8191ef30e89a9d
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-05-08 01:11:27 +0200

    Create exercice2.py

---
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/01/corrige/exercice2.py
@@ -0,0 +1,16 @@
+def nombre_suivant(s):
+    '''Renvoie le nombre suivant de celui représenté par s
+    en appliquant le procédé de lecture.'''
+    resultat = ''
+    chiffre = s[0]
+    compte = 1
+    for i in range(1, len(s)): 
+        if s[i] == chiffre:
+            compte += 1
+        else:
+            resultat += str(compte) + chiffre
+            chiffre = s[i]
+            compte = 1
+    lecture_chiffre = str(compte) + chiffre
+    resultat += lecture_chiffre
+    return resultat
