c4badf66edd889d536fd50be895ea57c0fc3caed
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-05-11 18:29:39 +0200

    Update exercice1.py

--- TNSI/seances/pratique/html/consignes/sujets/2025-NSI/04/corrige/exercice1.py
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/04/corrige/exercice1.py
@@ -1,1 +1,23 @@
+def ecriture_binaire_entier_positif(n):
+    if n == 0:
+        return "0"
 
+    puissance_2 = 1
+    tab = []
+    while puissance_2 <= n:
+        tab.append(puissance_2)
+        puissance_2 *= 2
+
+    var = ""
+    for i in tab[::-1]:
+        if i <= n:
+            var += "1"
+            n -= i
+        else:
+            var += "0"
+
+    return var
+
+print(ecriture_binaire_entier_positif(0))
+print(ecriture_binaire_entier_positif(2))
+print(ecriture_binaire_entier_positif(105))
