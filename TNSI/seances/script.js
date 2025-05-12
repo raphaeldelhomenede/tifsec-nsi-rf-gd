1cd50f367122930e2e25c12ab98943b039e30672
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-05-12 17:11:07 +0200

    Update script.js

--- TNSI/seances/script.js
+++ TNSI/seances/script.js
@@ -4,18 +4,18 @@ function getQueryParam(param) {
 }
 
 fetch('data-fonts.json')
-    .then(response => response.json())
-    .then(fontStyles => {
-        let font = getQueryParam('font');
-        let link = document.createElement('link');
-        link.rel = 'stylesheet';
+.then(response => response.json())
+.then(fontStyles => {
+    let font = getQueryParam('font');
+    let link = document.createElement('link');
+    link.rel = 'stylesheet';
 
-        if (fontStyles[font]) {
-            link.href = fontStyles[font].link;
-        } else {
-            link.href = fontStyles["default"].link;
-        }
+    if (fontStyles[font]) {
+        link.href = fontStyles[font].link;
+    } else {
+        link.href = fontStyles["default"].link;
+    }
 
-        document.head.appendChild(link);
-    })
-    .catch(error => console.error("Erreur de chargement du JSON:", error));
+    document.head.appendChild(link);
+})
+.catch(error => console.error("Erreur de chargement du JSON:", error));
