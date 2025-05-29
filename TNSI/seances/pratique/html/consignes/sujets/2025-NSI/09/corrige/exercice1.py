e8b27309ddaea6186562f9c434b28df9fcaa6c80
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-05-29 21:03:20 +0200

    Update exercice1.py

---
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/09/corrige/exercice1.py
@@ -0,0 +1,21 @@
+def multiplication(n1, n2):
+    result = 0
+    if n2 > 0:
+        for _ in range(n2):
+            result += n1
+        return result
+    elif n2 < 0 and n1 < 0:
+        for _ in range(-n2):
+            result -= n1
+        return result
+    elif n2 > 0 > n1:
+        for _ in range(n2):
+            result += n1
+        return result
+    if n1 == 0 or n2 == 0:
+        return 0
+
+print(multiplication(3, 5))
+print(multiplication(-4, -8))
+print(multiplication(-2, 6))
+print(multiplication(-2, 0))
