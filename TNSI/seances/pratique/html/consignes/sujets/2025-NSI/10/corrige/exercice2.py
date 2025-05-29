d9dc2b229c819841f1fbdf1875737a600702fa93
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-05-29 22:13:24 +0200

    Update exercice2.py

---
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/10/corrige/exercice2.py
@@ -0,0 +1,20 @@
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def position_alphabet(lettre):
+    '''Renvoie la position de la lettre dans l'alphabet'''
+    return ord(lettre) - ord('A')
+
+def cesar(message, decalage):
+    '''Renvoie le message codé par la méthode de César
+    pour le decalage donné'''
+    resultat = ''
+    for c in message:
+        if 'A' <= c and c <= 'Z':
+            indice = (position_alphabet(c) + decalage) % 26
+            resultat = resultat + alphabet[indice]
+        else:
+            resultat = resultat + c
+    return resultat
+
+print(cesar('BONJOUR A TOUS. VIVE LA MATIERE NSI !', 4))
+print(cesar('GTSOTZW F YTZX. ANAJ QF RFYNJWJ SXN !', -5))
