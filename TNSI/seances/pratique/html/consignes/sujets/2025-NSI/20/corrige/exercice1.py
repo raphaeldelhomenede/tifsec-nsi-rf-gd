2bcde07f8d1737f64eaf71fd87331f33a3f375d2
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-05 21:53:10 +0200

    Add files via upload

--- TNSI/seances/pratique/html/consignes/sujets/2025-NSI/20/corrige/exercice1.py
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/20/corrige/exercice1.py
@@ -1,1 +1,15 @@
+def min_et_max(tab):
+    min_tab = 0
+    max_tab = 0
+    for i in range(len(tab)):
+        if tab[i] < tab[min_tab]:
+            min_tab = i
+        if tab[i] > tab[max_tab]:
+            max_tab = i
+    return {"min": tab[min_tab], "max": tab[max_tab]}
 
+print(min_et_max([0, 1, 4, 2, -2, 9, 3, 1, 7, 1]))
+print(min_et_max([0, 1, 2, 3]))
+print(min_et_max([3]))
+print(min_et_max([1, 3, 2, 1, 3]))
+print(min_et_max([-1, -1, -1, -1, -1]))
