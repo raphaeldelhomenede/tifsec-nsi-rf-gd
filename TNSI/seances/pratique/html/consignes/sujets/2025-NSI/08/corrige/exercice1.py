655a4723c271c864108b0b822ca98d9bb72d8314
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-05 21:15:47 +0200

    Add files via upload

--- TNSI/seances/pratique/html/consignes/sujets/2025-NSI/08/corrige/exercice1.py
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/08/corrige/exercice1.py
@@ -1,1 +1,15 @@
+def maximum_tableau(tab):
+    if len(tab) == 0:
+        return False
 
+    if not all(isinstance(i, int) for i in tab):
+        return False
+
+    valeur_max = tab[0]
+    for i in tab:
+        if i > valeur_max:
+            valeur_max = i
+    return valeur_max
+
+print(maximum_tableau([98, 12, 104, 23, 131, 9]))
+print(maximum_tableau([-27, 24, -3, 15]))
