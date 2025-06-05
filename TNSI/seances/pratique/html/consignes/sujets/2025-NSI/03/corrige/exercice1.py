d2ccd84f29f61ac60230bcf3e0d2a23175388d5a
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-05 21:09:39 +0200

    Add files via upload

--- TNSI/seances/pratique/html/consignes/sujets/2025-NSI/03/corrige/exercice1.py
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/03/corrige/exercice1.py
@@ -1,1 +1,9 @@
+def fibonacci(n):
+    if n < 3:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
+print(fibonacci(1))
+print(fibonacci(2))
+print(fibonacci(25))
---
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/03/corrige/exercice2.py
@@ -0,0 +1,16 @@
+def eleves_du_mois(eleves, notes):
+    note_maxi = 0
+    meilleurs_eleves = []
+
+    for i in range(len(notes)):
+        if notes[i] == note_maxi:
+            meilleurs_eleves.append(eleves[i])
+        elif notes[i] > note_maxi:
+            note_maxi = notes[i]
+            meilleurs_eleves = [eleves[i]]
+
+    return note_maxi, meilleurs_eleves
+
+eleves_nsi = ['a','b','c','d','e','f','g','h','i','j']
+notes_nsi = [30, 40, 80, 60, 58, 80, 75, 80, 60, 24]
+print(eleves_du_mois(eleves_nsi, notes_nsi))
