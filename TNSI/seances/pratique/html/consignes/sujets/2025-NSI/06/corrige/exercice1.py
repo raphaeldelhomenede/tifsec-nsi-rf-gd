07337f6e94283392172735ef2cf505a47b42646b
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-05 21:13:50 +0200

    Add files via upload

--- TNSI/seances/pratique/html/consignes/sujets/2025-NSI/06/corrige/exercice1.py
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/06/corrige/exercice1.py
@@ -1,1 +1,21 @@
+def liste_puissances(a, n):
+    liste_de_puissances = 1
+    tab = []
+    for _ in range(n):
+        liste_de_puissances *= a
+        tab.append(liste_de_puissances)
+    return tab
 
+def liste_puissances_borne(a, borne):
+    liste_de_puissances = 1
+    tab = []
+    while liste_de_puissances < borne:
+        tab.append(liste_de_puissances)
+        liste_de_puissances *= a
+    return tab[1:]
+
+print(liste_puissances(3, 5))
+print(liste_puissances(-2, 4))
+print(liste_puissances_borne(2, 16))
+print(liste_puissances_borne(2, 17))
+print(liste_puissances_borne(5, 5))
