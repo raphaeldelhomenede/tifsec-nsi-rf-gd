322485b5a4848186b861aa796b374ff9b016b729
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-05 21:54:32 +0200

    Add files via upload

--- TNSI/seances/pratique/html/consignes/sujets/2025-NSI/21/corrige/exercice1.py
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/21/corrige/exercice1.py
@@ -1,1 +1,14 @@
+def indices_maxi(tab):
+    maxi = tab[0]
+    for i in tab:
+        if i > maxi:
+            maxi = i
 
+    tab1 = []
+    for i in range(len(tab)):
+        if tab[i] == maxi:
+            tab1.append(i)
+    return maxi, tab1
+
+print(indices_maxi([1, 5, 6, 9, 1, 2, 3, 7, 9, 8]))
+print(indices_maxi([7]))
