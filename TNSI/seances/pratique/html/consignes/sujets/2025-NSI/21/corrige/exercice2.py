08e99f7e302af9989708dd9adc492aedd8e069a9
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-05 22:55:17 +0200

    Update exercice2.py

--- TNSI/seances/pratique/html/consignes/sujets/2025-NSI/21/corrige/exercice2.py
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/21/corrige/exercice2.py
@@ -2,31 +2,18 @@ def renverse(pile):
     '''renvoie une pile contenant les mêmes éléments que pile,
     mais dans l'ordre inverse.
     Cette fonction détruit pile.'''
-    copie = pile.copy()
-    pile_inverse = []
-    while copie:
-        pile_inverse.append(copie.pop())
-    return pile_inverse
+    pile_inverse = [] 
+    while pile != []:
+        pile_inverse.append(pile.pop()) 
+    return pile_inverse 
 
 
 def positifs(pile):
-    '''Renvoie une pile contenant les éléments positifs de pile, dans le même ordre.
-    Ne modifie pas la pile d'origine.'''
-    copie = pile.copy()
-    pile_positifs = []
-    temp = []
-
-    while copie:
-        valeur = copie.pop()
-        if valeur >= 0:
-            temp.append(valeur)
-
-    # remettre dans l'ordre original
-    while temp:
-        pile_positifs.append(temp.pop())
-
-    return pile_positifs
-
-print(renverse([1, 2, 3, 4, 5]))
-print(positifs([-1, 0, 5, -3, 4, -6, 10, 9, -8]))
-print(positifs([-2]))
+    '''renvoie une pile contenant les éléments positifs de pile,
+    dans le même ordre. Cette fonction détruit pile.'''
+    pile_positifs = [] 
+    while pile != []:
+        elt = pile.pop() 
+        if elt >= 0: 
+            pile_positifs.append(elt)
+    return renverse(pile_positifs)
