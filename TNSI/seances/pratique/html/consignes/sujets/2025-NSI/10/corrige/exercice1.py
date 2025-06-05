ea2691538becf3c12f28112667c856f84acfbe20
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-05 21:19:45 +0200

    Add files via upload

--- TNSI/seances/pratique/html/consignes/sujets/2025-NSI/10/corrige/exercice1.py
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/10/corrige/exercice1.py
@@ -1,1 +1,9 @@
+def recherche(tab, n):
+    attemps = 0
+    for i in tab:
+        if i == n:
+            return attemps
+        attemps += 1
 
+print(recherche([2, 3, 4, 5, 6], 5))
+print(recherche([2, 3, 4, 6, 7], 5))
