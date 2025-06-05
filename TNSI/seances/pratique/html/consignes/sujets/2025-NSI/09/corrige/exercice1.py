6f5b9994539534af60134c28eeb91863795b78a9
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-05 21:17:02 +0200

    Add files via upload

--- TNSI/seances/pratique/html/consignes/sujets/2025-NSI/09/corrige/exercice1.py
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/09/corrige/exercice1.py
@@ -1,1 +1,15 @@
+def multiplication(n1, n2):
+    if n1 < 0:
+        return -multiplication(-n1, n2)
+    if n2 < 0:
+        return -multiplication(n1, -n2)
 
+    result = 0
+    for _ in range(n2):
+        result += n1
+    return result
+
+print(multiplication(3, 5))
+print(multiplication(-4, -8))
+print(multiplication(-2, 6))
+print(multiplication(-4, 0))
