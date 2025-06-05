b057d13bbe9cb6fee2f71b3a87ead12d8f97afe3
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-05 21:14:49 +0200

    Add files via upload

--- TNSI/seances/pratique/html/consignes/sujets/2025-NSI/07/corrige/exercice1.py
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/07/corrige/exercice1.py
@@ -1,1 +1,11 @@
+def nbr_occurrences(caractere, chaine_de_caracteres):
+    nb_occurrences = 0
+    for i in chaine_de_caracteres:
+        if caractere == i:
+            nb_occurrences += 1
+    return nb_occurrences
 
+print(nbr_occurrences("o", "bonjour"))
+print(nbr_occurrences("b", "Bébé"))
+print(nbr_occurrences("B", "Bébé"))
+print(nbr_occurrences(" ", "Hello world !"))
