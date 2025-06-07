a61f79a3628671c624f90eb345634c27e0f579d6
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-07 23:19:25 +0200

    Update exercice1.py

--- TNSI/seances/pratique/html/consignes/sujets/2025-NSI/26/corrige/exercice1.py
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/26/corrige/exercice1.py
@@ -1,12 +1,17 @@
 def ajoute_dictionnaires(d1, d2):
+    d0 = []
     d = {}
-    for cle in d1:
-        d[cle] = d1[cle]
-    for cle in d2:
-        if cle in d:
-            d[cle] += d2[cle]
+    for i, j in d1.items():
+        d0.append((i, j))
+    for i, j in d2.items():
+        d0.append((i, j))
+
+    for i, j in d0:
+        if i in d:
+            d[i] += j
         else:
-            d[cle] = d2[cle]
+            d[i] = j
+
     return d
 
 print(ajoute_dictionnaires({1: 5, 2: 7}, {2: 9, 3: 11}))
