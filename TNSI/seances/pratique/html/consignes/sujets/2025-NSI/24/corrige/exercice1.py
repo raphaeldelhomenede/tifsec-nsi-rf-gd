6b6488b21c2ed73e3aaa34ebdbd3035eb14474d9
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-07 13:44:04 +0200

    Update exercice1.py

--- TNSI/seances/pratique/html/consignes/sujets/2025-NSI/24/corrige/exercice1.py
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/24/corrige/exercice1.py
@@ -1,1 +1,12 @@
+def enumere(tab):
+    enumere = {}
+    for i in range(len(tab)):
+        if tab[i] in enumere:
+            enumere[tab[i]].append(i)
+        else:
+            enumere[tab[i]] = [i]
+    return enumere
 
+print(enumere([]))
+print(enumere([1, 2, 3]))
+print(enumere([1, 1, 2, 3, 2, 1]))
