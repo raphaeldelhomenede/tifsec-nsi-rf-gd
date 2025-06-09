edf4b4c7eceb30c2c3d4c2a797516331b2dd0241
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-09 15:59:16 +0200

    Update exercice1.py

---
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/34/corrige/exercice1.py
@@ -0,0 +1,10 @@
+def tri_selection(tab):
+    for i in range(len(tab)-1):
+        indice_min = i
+        for j in range(i+1, len(tab)):
+            if tab[j] < tab[indice_min]:
+                indice_min = j
+        tab[i], tab[indice_min] = tab[indice_min], tab[i]
+    return tab
+
+print(tri_selection([1, 52, 6, -9, 12]))
