fbf6e5c0898ed3a66087e648ea1ee7a000c2a62a
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-09 12:28:18 +0200

    Update exercice2.py

---
+++ TNSI/seances/pratique/html/consignes/sujets/2025-NSI/32/corrige/exercice2.py
@@ -0,0 +1,14 @@
+valeurs = [100, 50, 20, 10, 5, 2, 1]
+
+def rendu_glouton(a_rendre, rang):
+    if a_rendre == 0:
+        return []
+    v = valeurs[rang]
+    if v <= a_rendre:
+        return [v] + rendu_glouton(a_rendre - v, rang)
+    else:
+        return rendu_glouton(a_rendre, rang+1)
+
+print(rendu_glouton(67, 0))
+print(rendu_glouton(291, 0))
+print(rendu_glouton(291,1))
