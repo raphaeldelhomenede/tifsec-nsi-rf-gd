522e4988131804cebbdf32209abb1e7066f16b11
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-21 00:43:42 +0200

    Create img_image.php

---
+++ TNSI/seances/img_image.php
@@ -0,0 +1,8 @@
+<?php
+$variable242 = $seances_data3[6][3][0][0][5][1][$session_actuelle][$font1];
+?>
+
+<img
+    src="<?= htmlspecialchars($variable242); ?>"
+    alt="<?= htmlspecialchars($variable242); ?>"
+>
