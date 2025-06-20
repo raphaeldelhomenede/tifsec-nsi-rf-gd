f5fb8867b048cb7f0f6040ce13abbb44a0688baf
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-21 00:23:54 +0200

    Create joyeuxanniversaire.php

---
+++ TNSI/seances/joyeuxanniversaire.php
@@ -0,0 +1,10 @@
+<div style="position: relative; width: 100%; height: 0; padding-bottom: 56.25%;">
+    <iframe
+        src="https://www.youtube-nocookie.com/embed/HjXxvooa-0g?controls=0"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+        style="position: absolute; top: -30px; left: -15px; width: 100%; height: 85%;">
+    </iframe>
+</div>
