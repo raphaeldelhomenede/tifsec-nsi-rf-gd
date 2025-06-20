4088638e7c8a7246ad22b48ec25cd65be7d0f3bf
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-21 00:38:32 +0200

    Update iframe-fonts1.php

--- TNSI/seances/iframe-fonts1.php
+++ TNSI/seances/iframe-fonts1.php
@@ -1,10 +1,4 @@
-<?php $variable242 = $seances_data3[6][2][$session_actuelle][$font1]; ?>
-<iframe
-    src="<?= htmlspecialchars($variable242); ?>"
-    class="tifsec1"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    referrerpolicy="strict-origin-when-cross-origin"
-    allowfullscreen
-    style="margin-left: -12px;">
-</iframe>
+<?php
+$path = $seances_data3[6][2][$session_actuelle][$font1];
+echo @file_get_contents($path);
+?>
