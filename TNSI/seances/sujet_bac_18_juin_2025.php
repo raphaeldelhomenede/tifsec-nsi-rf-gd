e22f5f9f65e6611507ca8ecd81c5070c76f24e0a
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-21 13:22:17 +0200

    Update sujet_bac_18_juin_2025.php

--- TNSI/seances/sujet_bac_18_juin_2025.php
+++ TNSI/seances/sujet_bac_18_juin_2025.php
@@ -1,5 +1,7 @@
 <?php
-$pdfContent = @file_get_contents("https://raphaeldelhomenede.github.io/tifsec-nsi-rf-gd/TNSI/Programme_NSI_Tle_fichiers/25_NSIJ2ME1.pdf");
+$encodedUrl = base64_decode("aHR0cHM6Ly9yYXBoYWVsZGVsaG9tZW5lZGUuZ2l0aHViLmlvL3RpZnNlYy1uc2ktcmYtZ2QvVE5TSS9Qcm9ncmFtbWVfTlNIX1RsZV9maWNoaWVycy8yNV9OU0lKMk1FMS5wZGY=");
+$pdfContent = @file_get_contents($encodedUrl);
+
 if ($pdfContent === false) {
     die("Erreur : impossible de récupérer le fichier PDF.");
 }
