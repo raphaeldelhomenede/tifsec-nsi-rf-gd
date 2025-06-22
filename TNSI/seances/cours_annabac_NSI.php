c8d2417eb69890877fffa3a9445aa4c736a8d176
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-06-22 21:23:05 +0200

    Update cours_annabac_NSI.php

--- TNSI/seances/cours_annabac_NSI.php
+++ TNSI/seances/cours_annabac_NSI.php
@@ -1,74 +1,8 @@
 <?php
-$session = $_GET['session'] ?? null;
-$theme = $_GET['theme'] ?? null;
-$chapitre = $_GET['chapitre'] ?? null;
+$content = @file_get_contents("https://www.google.com");
 
-$urls = [
-    "langages" => [
-        1 => "https://raphaeldelhomenede.github.io/tifsec-nsi-rf-gd/TNSI/seances/cours_annabac_NSI/Langages_programmation_et_algorithmique_Chapitre_1.html",
-        2 => "https://raphaeldelhomenede.github.io/tifsec-nsi-rf-gd/TNSI/seances/cours_annabac_NSI/Langages_programmation_et_algorithmique_Chapitre_2.html",
-        3 => "https://raphaeldelhomenede.github.io/tifsec-nsi-rf-gd/TNSI/seances/cours_annabac_NSI/Langages_programmation_et_algorithmique_Chapitre_3.html"
-    ],
-    "structures" => [
-        1 => "https://raphaeldelhomenede.github.io/tifsec-nsi-rf-gd/TNSI/seances/cours_annabac_NSI/Structures_de_données_Chapitre_1.html",
-        2 => "https://raphaeldelhomenede.github.io/tifsec-nsi-rf-gd/TNSI/seances/cours_annabac_NSI/Structures_de_données_Chapitre_2.html",
-        3 => "https://raphaeldelhomenede.github.io/tifsec-nsi-rf-gd/TNSI/seances/cours_annabac_NSI/Structures_de_données_Chapitre_3.html"
-    ],
-    "bases" => [
-        1 => "https://raphaeldelhomenede.github.io/tifsec-nsi-rf-gd/TNSI/seances/cours_annabac_NSI/Conception_de_bases_de_données_Chapitre_1.html",
-        2 => "https://raphaeldelhomenede.github.io/tifsec-nsi-rf-gd/TNSI/seances/cours_annabac_NSI/Conception_de_bases_de_données_Chapitre_2.html"
-    ],
-    "architectures" => [
-        1 => "https://raphaeldelhomenede.github.io/tifsec-nsi-rf-gd/TNSI/seances/cours_annabac_NSI/Composants_et_processus_Chapitre1.html",
-        2 => "https://raphaeldelhomenede.github.io/tifsec-nsi-rf-gd/TNSI/seances/cours_annabac_NSI/Composants_et_processus_Chapitre2.html"
-    ],
-    "avancee" => [
-        1 => "https://raphaeldelhomenede.github.io/tifsec-nsi-rf-gd/TNSI/seances/cours_annabac_NSI/Programmation_avancée_et_algorithmique_Chapitre_1.html",
-        2 => "https://raphaeldelhomenede.github.io/tifsec-nsi-rf-gd/TNSI/seances/cours_annabac_NSI/Programmation_avancée_et_algorithmique_Chapitre_2.html"
-    ]
-];
-
-if ($theme && $chapitre && isset($urls[$theme][(int)$chapitre])) {
-    echo @file_get_contents($urls[$theme][(int)$chapitre]);
+if ($content === false) {
+    echo "Problème avec file_get_contents !";
+} else {
+    echo "Ça fonctionne !";
 }
-?>
-
-<style>
-    ul { line-height: 1.6; }
-    a { text-decoration: none; color: #2980b9; }
-</style>
-
-<h1>📘 Présentation générale du cours NSI</h1>
-<p>Ce site regroupe les différents chapitres du programme NSI (Numérique et Sciences Informatiques).</p>
-
-<h2>Langages, programmation et algorithmique</h2>
-<ul>
-    <li><a href="?session=cours_annabac_NSI&theme=langages&chapitre=1">Chapitre 1 : Modularité et mise au point des programmes</a></li>
-    <li><a href="?session=cours_annabac_NSI&theme=langages&chapitre=2">Chapitre 2 : Récursivité</a></li>
-    <li><a href="?session=cours_annabac_NSI&theme=langages&chapitre=3">Chapitre 3 : Paradigmes de programmation</a></li>
-</ul>
-
-<h2>Structures de données</h2>
-<ul>
-    <li><a href="?session=cours_annabac_NSI&theme=structures&chapitre=1">Chapitre 1 : Interface et implémentation des structures de données</a></li>
-    <li><a href="?session=cours_annabac_NSI&theme=structures&chapitre=2">Chapitre 2 : Structures arborescentes</a></li>
-    <li><a href="?session=cours_annabac_NSI&theme=structures&chapitre=3">Chapitre 3 : Graphes</a></li>
-</ul>
-
-<h2>Bases de données</h2>
-<ul>
-    <li><a href="?session=cours_annabac_NSI&theme=bases&chapitre=1">Chapitre 1 : Conception de bases de données</a></li>
-    <li><a href="?session=cours_annabac_NSI&theme=bases&chapitre=2">Chapitre 2 : Systèmes de gestion de bases de données – SQL</a></li>
-</ul>
-
-<h2>Architectures matérielles, systèmes d'exploitation et réseaux</h2>
-<ul>
-    <li><a href="?session=cours_annabac_NSI&theme=architectures&chapitre=1">Chapitre 1 : Composants et processus</a></li>
-    <li><a href="?session=cours_annabac_NSI&theme=architectures&chapitre=2">Chapitre 2 : Protocoles de routage et sécurisation des communications</a></li>
-</ul>
-
-<h2>Programmation avancée et algorithmique</h2>
-<ul>
-    <li><a href="?session=cours_annabac_NSI&theme=avancee&chapitre=1">Chapitre 1 : Programmes et données – Calculabilité</a></li>
-    <li><a href="?session=cours_annabac_NSI&theme=avancee&chapitre=2">Chapitre 2 : Programmation avancée</a></li>
-</ul>
