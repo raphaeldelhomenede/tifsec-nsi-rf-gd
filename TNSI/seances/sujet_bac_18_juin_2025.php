f089e4b56cf0283001f6cce422359e1e421ba499
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-07-05 12:49:45 +0200

    Update sujet_bac_18_juin_2025.php

--- TNSI/seances/sujet_bac_18_juin_2025.php
+++ TNSI/seances/sujet_bac_18_juin_2025.php
@@ -9,25 +9,19 @@ if ($pdfContent === false || $pdfContent1 === false) {
 $dataURI = "data:application/pdf;base64," . base64_encode($pdfContent);
 $dataURI1 = "data:application/pdf;base64," . base64_encode($pdfContent1);
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Sujet et correction NSI Bac 2025</title>
-    <style>
-        iframe {
-            width: 100%;
-            height: 95vh;
-            border: none;
-            margin-bottom: 2rem;
-        }
-    </style>
-</head>
-<body>
-    <h2>Le sujet bac de NSI du 18 juin 2025 en France Métropolitaine :</h2>
-    <iframe src="<?php echo $dataURI; ?>" title="Sujet NSI Bac 2025"></iframe>
 
-    <h2>La correction du sujet :</h2>
+<style>
+    iframe {
+        width: 100%;
+        height: 95vh;
+        border: none;
+        margin-bottom: 2rem;
+    }
+</style>
+
+<iframe src="<?php echo $dataURI; ?>" title="Sujet NSI Bac 2025"></iframe>
+
+<?php if (isset($_GET['corrigé'])): ?>
+    <h2>Correction du sujet</h2>
     <iframe src="<?php echo $dataURI1; ?>" title="Correction NSI Bac 2025"></iframe>
-</body>
-</html>
+<?php endif; ?>
