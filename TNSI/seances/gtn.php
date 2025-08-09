bdfafa736965b2b44169038e14717627a20895a7
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-08-10 01:00:35 +0200

    Update gtn.php

--- TNSI/seances/gtn.php
+++ TNSI/seances/gtn.php
@@ -1,4 +1,10 @@
 <?php
+    $nb_min = 0;
+    $nb_max = 10**9;
+    $pp_bot = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/ChatGPT-Logo.svg/512px-ChatGPT-Logo.svg.png";
+    $pp_user = "https://i.imgur.com/uCkGBeA.png";
+    $recompense1 = "https://tifsec-nsi.rf.gd/TNSI/session/ancienne_version_terminale";
+    $recompense2 = "https://www.youtube-nocookie.com/embed/--UcD80SBXM?autoplay=1&loop=1&playlist=--UcD80SBXM&controls=0&modestbranding=1&mute=1&showinfo=0&vq=hd720&rel=0";
     if (!isset($_SESSION['nombre_a_trouver'])) {
         $_SESSION['nombre_a_trouver'] = rand($nb_min, $nb_max);
         $_SESSION['tentatives'] = 0;
@@ -159,4 +165,5 @@
             }
         });
     </script>
+
 </div>
