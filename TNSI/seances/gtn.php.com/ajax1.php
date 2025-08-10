b7a7f94d8a1c1b0d7de73c76c52c20bed635e5cc
Raphael del <officielraphaeldelhomenede@gmail.com>
2025-08-10 19:07:58 +0200

    Update ajax1.php

--- TNSI/seances/gtn.php.com/ajax1.php
+++ TNSI/seances/gtn.php.com/ajax1.php
@@ -95,12 +95,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     $_SESSION['messages'][] = ['bot', "Trop grand 📈"];
                 } else {
                     $tentatives = $_SESSION['tentatives'];
-                    $download_link = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] . "?session=gtn.php.com&download=1";
-
+                    $download_link = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://') 
+                        . $_SERVER['HTTP_HOST'] 
+                        . $_SERVER['PHP_SELF'] 
+                        . "?session=gtn.php.com&download=1";
+                    
                     $message_final = ($tentatives <= 40)
-                        ? "<a href='$recompense1'>Félicitations</a> 🎉 Tu as trouvé le nombre " . number_format($nombre_a_trouver, 0, '.', ' ') . " en $tentatives tentatives !<br><a href='$download_link'>📥 Télécharger la partie (JSON)</a><br>Tu peux maintenant <button class='button' onclick="arreter()">Rejouer ou réinitialiser</button>."
-                        : "<a href='$recompense2'>Bravo</a> 🎉 Tu as trouvé le nombre " . number_format($nombre_a_trouver, 0, '.', ' ') . " en $tentatives tentatives !<br><a href='$download_link'>📥 Télécharger la partie (JSON)</a>";
-
+                        ? "<a href='$recompense1'>Félicitations</a> 🎉 Tu as trouvé le nombre " 
+                        . number_format($nombre_a_trouver, 0, '.', ' ') 
+                        . " en $tentatives tentatives !<br>"
+                        . "<a href='$download_link'>📥 Télécharger la partie (JSON)</a><br>"
+                        . "Tu peux maintenant <button class='button' onclick=\"arreter()\">Rejouer ou réinitialiser</button>."
+                        : "<a href='$recompense2'>Bravo</a> 🎉 Tu as trouvé le nombre " 
+                        . number_format($nombre_a_trouver, 0, '.', ' ') 
+                        . " en $tentatives tentatives !<br>"
+                        . "<a href='$download_link'>📥 Télécharger la partie (JSON)</a>" .
+                        "Tu peux maintenant <button class='button' onclick=\"arreter()\">Rejouer ou réinitialiser</button>.";
+                    
                     $_SESSION['messages'][] = ['bot', $message_final];
 
                     echo json_encode([
